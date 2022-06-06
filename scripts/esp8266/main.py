92aac4e59217604ec8b8eea2ffd0edb658bd702a
Artem Vesnin <artemvesnin@gmail.com>
2022-06-06 11:17:46 +0000

    Added local play for esp8266

--- scripts/esp8266/main.py
+++ scripts/esp8266/main.py
@@ -1,15 +1,28 @@
+import os
 import leds_control
 
-def run():
+def run(local=False, do_move=True):
     IP = '192.168.4.2'
+    motion={'dir': 'down', 'speed': 20}
     fpath, nbytes, size = leds_control.load_image(IP)
-    if nbytes != size[0] * size[1] * 3:
-        #print("The bytes load not equal image size")
-        #print("Resubmit image on web and try again")
-        leds_control.show_no_image_leds()
+    if not local:
+        if nbytes != size[0] * size[1] * 3:
+            #print("The bytes load not equal image size")
+            #print("Resubmit image on web and try again")
+            leds_control.show_no_image_leds()
+        else:
+            if do_move:
+                leds_control.show(size, delay=0.0, motion=motion)
+            else:
+                leds_control.show(size, delay=0.0, motion={})
     else:
-        motion={'dir': 'down', 'speed': 20}
-        leds_control.show(size, delay=0.0, motion=motion)
+        if leds_control.FILE in os.listdir():
+            if do_move:
+                leds_control.show(size, delay=0.0, motion=motion)
+            else:
+                leds_control.show(size, delay=0.0, motion={})
 
-run()    
+while True:
+    run()    
+    
 
