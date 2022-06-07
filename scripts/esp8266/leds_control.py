5b84bbbd30c8fdaab02a4cfdb18fdd23b2e1360d
Artem Vesnin <artemvesnin@gmail.com>
2022-06-07 02:36:59 +0000

    Added infinite image play for last loaded (with checks updates)

--- scripts/esp8266/leds_control.py
+++ scripts/esp8266/leds_control.py
@@ -2,7 +2,9 @@ from machine import Pin
 from neopixel import NeoPixel
 import utime 
 import urequests as req
+import os
 from stepper import STEPPER
+
 
 #IP = '0.0.0.0'
 FILE = 'pixels.pix'
@@ -16,6 +18,7 @@ STEPPER_CONFIG = {'In1':5,
                   'max_speed': 20 } 
 MOVE_LINE_STEPS = 17.6367
 NLEDS_STRIP = 144
+last_size = (NLEDS_STRIP, 1)
 stepper = STEPPER(STEPPER_CONFIG) 
 pix = NeoPixel(Pin(2), NLEDS_STRIP)
 
@@ -29,12 +32,11 @@ def show_no_image_leds():
             pix[i] = (0,0,0)
         pix.write()
 
-    
 
 def load_image(IP, PORT = '5000', fname=FILE):
-    with open(fname, 'wb') as f:
-        pass
     size = 0
+    aspect = [0, 0]
+    first = True
     while True:
         resp = req.get('http://'+IP+':'+PORT+'/get_portion')
         if resp.status_code == 500:
@@ -45,19 +47,35 @@ def load_image(IP, PORT = '5000', fname=FILE):
             data = resp.content
             if len(data) == 0:
                 break
+            if first:
+                with open(fname, 'wb') as f:
+                    pass
+                resp_size = req.get('http://'+IP+':'+PORT+'/get_size')
+                aspect = resp_size.json()['size']
+                first = False
             size += len(data)   
             with open(fname, 'ab') as f:
                 f.write(data)
             while True:
-                resp = req.post('http://'+IP+':'+PORT+'/next_portion')
-                if resp.status_code == 200:
+                resp_next = req.post('http://'+IP+':'+PORT+'/next_portion')
+                if resp_next.status_code == 200:
                     break
+            
         else:
             print('Unknow error, see server logs')
             utime.sleep(1)
-    resp = req.get('http://'+IP+':'+PORT+'/get_size')
-    aspect = resp.json()['size']
     return fname, size, aspect
+
+def get_local_size(nleds):
+    height = 0
+    with open(FILE, 'rb') as f:
+        while True:
+            data = f.read(nleds * 3)
+            if data == nleds * 3:
+                height += 1
+            else:
+                break
+    return (nleds, height)
     
 def move(steps, speed,  moveup=False):
     stepper.on()
@@ -67,7 +85,7 @@ def move(steps, speed,  moveup=False):
     stepper.off()
     return utime.time() - start
 
-def show(size, delay=0.02, motion={}, lines=LINES, scaler=8):
+def show(size, delay=0.02, motion={}, lines=LINES, scaler=8, RGB=True):
     s = size
     NLEDS = s[0]
     HEIGHT = s[1]
@@ -84,7 +102,10 @@ def show(size, delay=0.02, motion={}, lines=LINES, scaler=8):
                 for iled in range(NLEDS):
                     ind = (iled + line * NLEDS) * 3
                     r, g, b = data[ind:ind+3]
-                    pix[iled] = (int(g/scaler), int(r/scaler), int(b/scaler)) #data[ind:ind+3]
+                    if RGB:
+                        pix[iled] = (int(r/scaler), int(g/scaler), int(b/scaler))
+                    else:
+                        pix[iled] = (int(g/scaler), int(r/scaler), int(b/scaler)) #data[ind:ind+3]
                 pix.write()
                 if motion:
                     moveup = True if motion['dir'] == 'up' else  False
@@ -94,3 +115,28 @@ def show(size, delay=0.02, motion={}, lines=LINES, scaler=8):
                     utime.sleep(delay)
                 lastline += 1
 
+
+def run(IP, local=False, do_move=False, RGB=True):    
+    global last_size
+    motion={'dir': 'down', 'speed': 20}
+    fpath, nbytes, size = load_image(IP)
+    size_ok = (nbytes == size[0] * size[1] * 3) and size[1] > 1
+
+    if (not size_ok) and local:
+        image_exists = False
+        _bytes = last_size[0] * last_size[1] * 3
+        for f in os.listdir():
+            if f == FILE:
+                image_exists = os.stat(f)[-4] == _bytes
+                break
+        if image_exists:
+            if do_move:
+                show(last_size, delay=0.0, motion=motion, RGB=RGB)
+            else:
+                show(last_size, RGB=RGB)
+    else:
+        last_size = size if size_ok else last_size
+        if do_move:
+            show(size, delay=0.0, motion=motion, RGB=RGB)
+        else:
+            show(size, delay=0.0, motion={}, RGB=RGB)
